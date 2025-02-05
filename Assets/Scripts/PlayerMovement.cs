306a0f9a3610a785fa13f9120df477c13428dc43
shenzhiqiang <593446878@qq.com>
2025-02-05 21:12:26 +0800

    提交。

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,13 +12,55 @@ public class PlayerMovement : MonoBehaviour
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
 
+    [SerializeField]
+    private Vector3 velocity;
+
+    [SerializeField]
+    private bool isGrounded;
+    [SerializeField]
+    private bool isMoving;
+
+    [SerializeField]
+    private CharacterController controller;
+    [SerializeField]
+    private Vector3 lastPosition = new Vector3(0, 0, 0);
+
     void Start()
     {
-
+        controller = GetComponent<CharacterController>();
     }
 
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2;
+        }
 
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        if (lastPosition != gameObject.transform.position && isGrounded == true)
+        {
+            isMoving = true;
+            // 等会用
+        }
+        else
+        {
+            isMoving = false;
+            // 等会用
+        }
+
+        lastPosition = gameObject.transform.position;
     }
 }
